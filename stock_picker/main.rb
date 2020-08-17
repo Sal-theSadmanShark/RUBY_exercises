cf0d680ec8027b5ca6140505cf77861b47a1390a
Salim Sadman <mohammadfahim911@gmail.com>
2020-08-18 00:21:08 +0600

    Update main.rb

--- stock_picker/main.rb
+++ stock_picker/main.rb
@@ -1,10 +1,10 @@
 def stock_picker list
 	temp = 0
 	ans = Hash.new
-	list.each_with_index do |v,i| 
-				temp = v
-				list[i] = 0
-				list.each { |v| ans[[temp,v]] = v-temp if v > temp }					
+	list.each_with_index do |val,ind| 
+				temp = val
+				list[ind] = 0
+				list.each_with_index { |v,i| ans[[ind,i]] = v-temp if v > temp }					
 			end
 	ans = ans.sort_by {|k,v|  v}
 	ans.last[0]
